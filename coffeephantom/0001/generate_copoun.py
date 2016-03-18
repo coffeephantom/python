6b4c5bdd7a39a01a86dd448a549da2ac8c77bd12
coffeephantom <jfyy00@126.com>
2016-03-18 14:19:30 +0800

    fix the bug that copoun could be duplicated

--- coffeephantom/0001/generate_copoun.py
+++ coffeephantom/0001/generate_copoun.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-import random
 
 
 # 题目中对于激活码的格式并没有要求，目前采取这种策略
 def generate_copoun():
     copoun = []
-    copoun.append(str(random.randint(0, 200)))
+    for number in range(0, 200):
+        copoun.append(str(number))
     return copoun
