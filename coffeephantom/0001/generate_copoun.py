8ee696ca426c9503aab0007dd227f204aec7303e
coffeephantom <jfyy00@126.com>
2016-03-21 19:52:27 +0800

    update implementation

--- coffeephantom/0001/generate_copoun.py
+++ coffeephantom/0001/generate_copoun.py
@@ -1,9 +1,27 @@
 # -*- coding: utf-8 -*-
 
+import string
+import random
 
-# 题目中对于激活码的格式并没有要求，目前采取这种策略
 def generate_copoun():
-    copoun = []
-    for number in range(0, 200):
-        copoun.append(str(number))
-    return copoun
+    copoun_list = []
+    element = []
+    character = string.ascii_letters
+    digit = string.digits
+    for letter in character:
+        element.append(letter)
+
+    for num in digit:
+        element.append(num)
+    print element
+    
+    for i in range(0, 200):
+        copoun = ''
+        for count in range(0, 15):
+            copoun += random.choice(element)
+        if copoun not in copoun_list:
+            copoun_list.append(copoun)
+    
+    return copoun_list
+
+print generate_copoun()
